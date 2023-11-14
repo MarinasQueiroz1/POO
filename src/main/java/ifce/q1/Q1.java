b89427e9b840baed92ee4a906fe063a7f38b4a51
Marina <>
2023-11-14 15:36:26 -0300

    COnversor

---
+++ src/main/java/ifce/q1/Q1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ifce.q1;
+
+import javax.swing.JOptionPane;
+import modelo.Conversor;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Q1 {
+
+    public static void main(String[] args) {
+        Conversor c1 = new Conversor();
+        String real = JOptionPane.show
+        
+    }
+}
